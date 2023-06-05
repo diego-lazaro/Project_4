6e88a03dc54daed5a4cd559e5dabd27c6acaf029
aharris206 <119907767+aharris206@users.noreply.github.com>
2023-06-05 05:05:46 -0700

    Finished App and Models

---
+++ app.py
@@ -0,0 +1,50 @@
+import folium
+import pickle
+import streamlit as st
+
+from streamlit_folium import st_folium
+
+user_lat = st.number_input('Latitude', value=38.54, min_value=24.5, max_value=50.0, step=(1/3600))
+lat = int(round(user_lat, 1))
+lat_min = ((int(round((user_lat*60), 1)))-(lat*60))
+lat_sec = ((int(round((user_lat*3600), 1)))-(lat_min*60)-(lat*3600))
+st.write('Latitude: ', lat, '°', lat_min, '′', lat_sec, '″')
+
+user_lon = st.number_input('Longitude', value=-121.75, min_value=-125.0, max_value=-67.0, step=(1/3600))
+lon = int(round(user_lon, 1))
+lon_min = ((int(round((user_lon*60), 1)))-(lon*60))
+lon_sec = ((int(round((user_lon*3600), 1)))-(lon_min*60)-(lon*3600))
+st.write('Longitude: ', lon, '°', lon_min, '′', lon_sec, '″')
+
+# center & add marker
+m = folium.Map(location=[user_lat, user_lon], zoom_start=16)
+folium.Marker(
+    [user_lat, user_lon], popup="You are Here", tooltip="You are Here"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
+
+with open('Resources/Trained_Models/oldStandard.pkl', 'rb') as f:
+    oldStandard = pickle.load(f)
+
+with open('Resources/Trained_Models/current.pkl', 'rb') as f:
+    current = pickle.load(f)
+
+with open('Resources/Trained_Models/under5.pkl', 'rb') as f:
+    under5 = pickle.load(f)
+
+over50 = oldStandard.predict([[user_lat, user_lon]])
+over10 = current.predict([[user_lat, user_lon]])
+over5 = under5.predict([[user_lat, user_lon]])
+
+if (over50 == 1):
+    st.write('<h3>The Arsenic Contamination of the Groundwater at this location is very high!</h3><center><h1>(over 50㎍/L)</h1></center></br><h5>This exceeds the Environmental Protection Agency\'s orignal arsenic standard set in 1942!!</h5>', unsafe_allow_html=True)
+else:
+    if (over10 == 1):
+        st.write('<h3>The Arsenic Contamination of the Groundwater at this location is high!</h3><center><h3>(over 10㎍/L but under 50㎍/L)</h3></center></br><h5>This exceeds the Environmental Protection Agency\'s arsenic standard as of January 2006</h5>', unsafe_allow_html=True)
+    else:
+        if (over5 == 1):
+            st.write('<h3>The Arsenic Contamination of the Groundwater at this location is . . .</h3><center><h1>(between 5㎍/L and 10㎍/L)</h1></center></br><h5>This meets the Environmental Protection Agency\'s arsenic standard as of January 2006</h5>', unsafe_allow_html=True)
+        else:
+            st.write('<h3>The Arsenic Contamination of the Groundwater at this location is . . .</h3><center><h1>(less than 5㎍/L)</h1></center></br><h5>This meets the Environmental Protection Agency\'s arsenic standard as of January 2006</h5>', unsafe_allow_html=True)
